69288bbaa39c05f7dbdbb168e12b5dea40d8550b
kamrul2006 <kamrulislamapurba@gmail.com>
2025-05-16 01:04:52 +0600

    Bassic setup Complate

--- app/page.js
+++ app/page.js
@@ -3,7 +3,11 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      Hello World
+      <h1 className="text-5xl bg-black text-white py-20">
+        Hello World
+
+      </h1>
     </div>
   );
 }
+
